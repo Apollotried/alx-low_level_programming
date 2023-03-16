d6f6cac0ee8c38ca1a9b3d24f2fc62af483f862b
Apollotried <safeapollogemail@gmail.com>
2023-03-16 12:45:38 -0700

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints to the error 
- * Return 1
+ * main - prints to the error
+ * Return: 1
  */
-int main()
+int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+	fprintf
+	(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
