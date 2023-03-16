ad2b3dff8ca7b66c07d1156e72c48dfa8cf2ae83
Apollotried <safeapollogemail@gmail.com>
2023-03-16 08:40:07 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - printing using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
