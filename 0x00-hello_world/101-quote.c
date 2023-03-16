381d342437dc52079b1917ff15226e236da55b18
Apollotried <safeapollogemail@gmail.com>
2023-03-16 12:29:41 -0700

    test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints to the error 
+ * Return 1
+ */
+int main()
+{
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return 1;
+}
