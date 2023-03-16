c963a828192a032e65391751f72783b24b5dfc93
Apollotried <safeapollogemail@gmail.com>
2023-03-16 09:22:54 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - a program that prints various sizes of different typess
+ * Return: 0
+ */
+int main(void)
+{
+	printf("the size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("the size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("the size of a long int: %lu byte(s)\n",
+	(unsigned long)sizeof(long int));
+	printf("the size of a char: %lu byte(s)\n",
+	(unsigned long)sizeof(long long int));
+	printf("the size of a float: %lu byte(s)\n",
+	(unsigned long)sizeof(float));
+}
