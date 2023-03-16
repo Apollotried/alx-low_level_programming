bbd4d0ceaf29e4d059093716b0e4a3f39a99ebbe
Apollotried <safeapollogemail@gmail.com>
2023-03-16 08:35:08 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ *main - printing using puts
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
