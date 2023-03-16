5aec38cdc668c15dcf4c833b4980f8ee187ccc9c
Apollotried <safeapollogemail@gmail.com>
2023-03-16 09:26:09 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("size of a long int: %lu byte(s)\n",
 	(unsigned long)sizeof(long int));
-	printf("size of a char: %lu byte(s)\n",
+	printf("size of a long long int: %lu byte(s)\n",
 	(unsigned long)sizeof(long long int));
 	printf("size of a float: %lu byte(s)\n",
 	(unsigned long)sizeof(float));
