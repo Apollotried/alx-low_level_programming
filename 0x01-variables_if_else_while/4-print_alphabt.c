5eafc3e221c000cf33b8bae7f40012392a901828
Apollotried <safeapollogemail@gmail.com>
2023-03-17 13:30:13 -0700

    test

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 97 ; i < 101 ; i++)
+	for (i = 97 ; i < 101 && i != 101 && i != 113 ; i++)
 	{
 		putchar(i);
-	};
-	for (j = 102 ; j < 113 ; j++)
-	{
-		putchar(j);
 	};
 	putchar('\n');
 	return (0);
