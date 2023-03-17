11cfcaf934d68241ad96f9c66a4271d62b7cdc00
Apollotried <safeapollogemail@gmail.com>
2023-03-17 16:08:10 -0700

    test

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ int main(void)
 	{
 		for (j = i + 1 ; j < 10 ; j++)
 		{
-			putchar(i);
-			putchar(j);
+			putchar(i + '0');
+			putchar(j + '0');
 			if (i == 8 && j == 9)
 			{
 				putchar('\n');
