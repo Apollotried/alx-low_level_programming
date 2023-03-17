da7dce2fb7a7a498073655c5260a8f5436c5cbb3
Apollotried <safeapollogemail@gmail.com>
2023-03-17 13:16:59 -0700

    test

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 99 ; i < 122 ; i++)
+	for (i = 97 ; i < 123 ; i++)
 	{
 		putchar(i);
 	};
 	for (j = 65 ; j < 90 ; j++)
         {
-                putchar(j);
-        };
+		putchar(j);
+	};
 	putchar('\n');
 	return (0);
 }
