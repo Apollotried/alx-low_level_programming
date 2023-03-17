a187b34a93a82c7ee87020bd190b828f1ca6b56a
Apollotried <safeapollogemail@gmail.com>
2023-03-17 15:01:12 -0700

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - numbers again
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 58 ; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	};
+	return (0);
+}
