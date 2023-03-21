d541eae2162edb1f064e914455f5951ab9454887
Apollotried <safeapollogemail@gmail.com>
2023-03-21 06:35:32 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - 1. I sometimes suffer from insomnia. And when I can't fall asleep,
+*  I play what I call the alphabet game1. I sometimes suffer from insomnia.
+* And when I can't fall asleep, I play what I call the alphabet game
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 97 ; i < 123 ; i++)
+{
+_putchar(i);
+};
+return (0);
+}
