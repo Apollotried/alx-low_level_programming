8a74c6da86d306a53460a3627baeaa9de83f57f9
Apollotried <safeapollogemail@gmail.com>
2023-03-21 09:27:54 -0700

    test

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - prints sign
+ * Return: int
+ * @n: the number to check
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	};
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	};
+	_putvhar('+');
+	return (1);
+}
