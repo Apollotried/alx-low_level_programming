e25e64ed36c8647e5cf71ad3cd00469195df00e5
Apollotried <safeapollogemail@gmail.com>
2023-03-21 11:10:24 -0700

    test

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - shows times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			_putchar(i * j + '0');
+			if (j == 9)
+			{
+				_putchar('$');
+				_putchar('\n');
+				continue;
+			};
+			_putchar(',');
+			_putchar(' ');
+		};
+	};
+}
