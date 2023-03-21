1ece5aa242d69d804a41e2fb74096f652e308684
Apollotried <safeapollogemail@gmail.com>
2023-03-21 07:30:17 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * 2-print_alphabet_x10 - prints alphabets 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int x = 0;
+int i;
+
+while (x < 10)
+{
+	for (i = 97 ; i < 123 ; i++)
+	}
+_putchar(i);
+};
+x++;
+};
+}
