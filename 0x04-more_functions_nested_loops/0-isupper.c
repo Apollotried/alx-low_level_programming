ba95ad19757abc66d85ea880fbeb7ebbfd90270f
Apollotried <safeapollogemail@gmail.com>
2023-03-23 09:18:28 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checkes if uppercase
+ * @c: the variable to manipulate
+ * Return: 1 if uppercase and 0 if not uppercase
+ */
+int _isupper(int c)
+{
+	return (isupper(c));
+}
