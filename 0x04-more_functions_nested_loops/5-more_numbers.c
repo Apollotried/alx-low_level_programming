e9c17220d875fa03600ee0f16457e082acaae3d2
Apollotried <safeapollogemail@gmail.com>
2023-03-23 14:29:07 -0700

    test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,23 @@
 #include "main.h"
+
 /**
-* more_numbers - prints to 14 , 10 times
-* Return: void
-*/
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
+ */
+
 void more_numbers(void)
 {
-	int i;
-	int c = 0;
 
-	while (c < 10)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 0 ; i < 15 ; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
-			if (i == 14)
-			{
-				_putchar('\n');
-			}
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		c++;
-	}
-	_putchar('\n');
+			_putchar('\n');
+		}
 }
