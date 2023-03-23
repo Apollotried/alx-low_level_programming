bf9cc1f1b79896a91d8c1ec45d720925e9864ff7
Apollotried <safeapollogemail@gmail.com>
2023-03-23 13:44:18 -0700

    test

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
 *print_numbers - print 0123456789
-* owned by Bwave ICT
 *Return: void
 */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0 ; c <= 9 ; c++)
 	{
-	_putchar(c);
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
