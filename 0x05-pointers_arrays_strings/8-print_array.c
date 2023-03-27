d4fb7ab0f766135c1580d0377c123d66706009ec
Apollotried <safeapollogemail@gmail.com>
2023-03-27 15:51:28 -0700

    test

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints array elements
+ * @a: the array
+ * @n: number of elements
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%c\n", a[i]);
+			continue;
+		};
+		printf("%c, ", a[i])
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
