8c04032075f6c8ebaba75d743e1898f6baf3b136
Apollotried <safeapollogemail@gmail.com>
2023-03-27 13:44:05 -0700

    test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resetes the value to 98
+ * @n: the pointer to manipulate
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(int c);
+void reset_to_98(int *n);
