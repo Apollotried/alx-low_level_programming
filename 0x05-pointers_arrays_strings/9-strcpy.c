6b7379ab31f13dcb35ba719557f898c3178b58e6
Apollotried <safeapollogemail@gmail.com>
2023-03-27 15:42:28 -0700

    test

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - makes a copy of a string
+ * @dest: destination
+ * @src: source
+ * Return: char *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
