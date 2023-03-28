92b4501a6f734d0ee55fee8cbc9ec21450e58cf9
Apollotried <safeapollogemail@gmail.com>
2023-03-27 17:12:28 -0700

    test

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: the string to manipulate
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		_putchar(str[i]);
+		i += 2
+	};
+}
