7affed0e3a2215aeda47aa9e0d37eb61bbe64aaf
Apollotried <safeapollogemail@gmail.com>
2023-03-29 13:16:29 -0700

    test

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compars 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
