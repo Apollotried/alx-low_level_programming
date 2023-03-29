f36aab091ef1b0a8f26fc8abcef2158f6566367d
Apollotried <safeapollogemail@gmail.com>
2023-03-29 12:52:05 -0700

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - cocnctainating
+ * @dest: the dedtination string
+ * @src: the source string
+ * Return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
