090c1c44d2117219d2ad9de56cf08d1bb34f6935
Apollotried <safeapollogemail@gmail.com>
2023-03-29 13:49:58 -0700

    test

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @c: the string
+ * Return: char*
+ */
+char *string_toupper(char *c)
+{
+	while (*c)
+	{
+		*c = *c.toupper(*c);
+		c++
+	}
+}
