6dfdc653112b61a6f5b05c23ffef029d01e58c1e
Apollotried <safeapollogemail@gmail.com>
2023-04-03 17:50:15 -0700

    test

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: the string
  * @accept: the set of bytes
- * Retrurn: char*
+ * Return: char*
  */
 char *_strpbrk(char *s, char *accept)
 {
