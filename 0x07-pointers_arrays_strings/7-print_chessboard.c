29432113743c74135dbc4eef4517aaa9e17fdf5b
Apollotried <safeapollogemail@gmail.com>
2023-04-03 18:05:04 -0700

    test

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,7 +12,12 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0 ; j < 8 ; j++)
 		{
-			putchar(a[i][j]);
+			_putchar(a[i][j]);
+			_putchar(',');
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
 		}
 	}
 }
