31e11551f636b971a7a7a2f405446e5dfa5416c9
Apollotried <safeapollogemail@gmail.com>
2023-04-03 17:41:59 -0700

    test

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  *_strchr - locates a character in a string
+ *  @s: the string
+ *  @c: the character
+ *  Return: char*
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
