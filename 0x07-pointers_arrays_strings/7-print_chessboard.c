39ca28cc5e9e870763e7ecf417c8500114dabf5f
Apollotried <safeapollogemail@gmail.com>
2023-04-03 18:01:58 -0700

    test

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: the array of 8 chaarcters
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			putchar(a[i][j]);
+		}
+	}
+}
