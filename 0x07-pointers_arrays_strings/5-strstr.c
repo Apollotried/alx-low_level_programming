e0144673f7072749ca79bee59935a14a07e224a2
Apollotried <safeapollogemail@gmail.com>
2023-04-03 17:53:20 -0700

    test

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - locates a sub string
+ * @haystack: the string to search
+ * @needle: the substring to locate
+ * Return: char*
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(*haystack, needle));
+}
