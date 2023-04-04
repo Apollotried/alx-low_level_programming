b5d3733cbf06469d9e1c7230d6d10e34b16491e2
Apollotried <safeapollogemail@gmail.com>
2023-04-03 19:21:02 -0700

    test

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,31 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - sum of the diag
- * @a: the array
- * @size: its size
+ * print_diagsums - entry
+ * @a: input
+ * @size: input
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int j;
-	int count = 0;
-	int count2 = 0;
+	int count1;
+	int count2;
+
+	count1 = 0;
+	count2 = 0;
 
 	for (i = 0 ; i < size ; i++)
 	{
-		for (j = 0 ; j < size ; j++)
-		{
-			if (i == j)
-			{
-				count += a[i * size + j];
-			};
-			if (i + j == size - 1)
-			{
-				count2 += a[i * size + j];
-			}
-		};
-	};
-	printf("%d, %d", count, count2);
+		count1 = count1 + a[i * size + i];
+	}
+	for (i = size - 1 ; i > size ; i--)
+	{
+		count2 = count2 + a[y * size + (size - y - 1)];
+	}
+	printf("%d, %d\n", count1, count2);
 }
