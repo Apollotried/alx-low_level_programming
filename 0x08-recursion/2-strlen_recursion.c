e0b0da785246fba0a4c71b6279949271ea3109a0
Apollotried <safeapollogemail@gmail.com>
2023-04-05 15:56:55 -0700

    test

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - rturns the length of a string
+ * @s: the string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		retun;
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
