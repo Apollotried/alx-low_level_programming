0f0f1be4e32bfb72bbe2a1aec43fb660416e126d
Apollotried <safeapollogemail@gmail.com>
2023-04-05 16:10:11 -0700

    test

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - x raised to the power of y
+ * @x: first number
+ * @y: second number
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	};
+	if (y == 1)
+	{
+		return (x);
+	};
+	return (x * _pow_recursion(y - 1));
+}
