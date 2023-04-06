450c62ed2ce24fbe85c7e5386129e18c120dfb95
Apollotried <safeapollogemail@gmail.com>
2023-04-05 18:10:11 -0700

    test

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - checkes if a number is prime
+ * @n: the number to manipulate
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	};
+	return (helper(n, 2));
+}
+/**
+ * helper - helps for the program
+ * @n: first param
+ * @i: second one
+ * Return: int
+ */
+
+int helper(int n, int i)
+{
+	if (n % i == 0)
+	{
+		return (0);
+	};
+	return (helper(n, i + 1));
+}
