d4ffdb758011527c315d09753ff03174a961edcb
Apollotried <safeapollogemail@gmail.com>
2023-04-09 15:29:06 -0700

    test

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: count
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
