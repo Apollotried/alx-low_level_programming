33ecc566170989a1b892e247500121f798633f36
Apollotried <safeapollogemail@gmail.com>
2023-04-11 17:33:18 -0700

    test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - entry
+ * @width: rows
+ * @height: cols
+ * Return: int**
+ */
+int **alloc_grid(int width, int height)
+{
+int **grid;
+int i;
+int j;
+
+if (width == 0 || height == 0)
+{
+return (NULL);
+};
+grid = (int **)malloc(height * sizeof(int));
+for (i = 0 ; i < height ; i++)
+{
+grid[i] = (int *)malloc(width * sizeof(int));
+};
+if (grid == NULL)
+return (NULL);
+for (i = 0 ; i < height ; i++)
+{
+for (j = 0 ; j < width ; j++)
+{
+grid[i][j] = 0;
+}
+}
+return (grid);
+}
