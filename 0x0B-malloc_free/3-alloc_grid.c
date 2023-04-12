be020a6d6c7bd7780c9bc71c133d726bb8522777
Apollotried <safeapollogemail@gmail.com>
2023-04-11 17:44:52 -0700

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,9 +17,17 @@ if (width == 0 || height == 0)
 return (NULL);
 };
 grid = (int **)malloc(height * sizeof(int *));
+
 for (i = 0 ; i < height ; i++)
 {
 grid[i] = (int *)malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+	for (j = 0 ; j < i ; j++)
+		free(grid[j]);
+	free(grid);
+	return (NULL);
+};
 };
 if (grid == NULL)
 return (NULL);
