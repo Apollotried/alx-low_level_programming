34d4267ab3d739da20c9ab5a443a7cbf5afc7181
Apollotried <safeapollogemail@gmail.com>
2023-04-13 12:48:06 -0700

    second task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concats 2 strings 
+ * @s1: destination
+ * @s2: source
+ * @n: maximum n from s2
+ * Return: char*
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	res = (char*) malloc(strlen(s1) + strlen(s2) + 1);
+	
+	if (res == NULL)
+		return (NULL);
+
+	strncat(res, s1, strlen(s1));
+	if (n >= strlen(s2))
+	{
+		strncat(res, s2, strlen(s2));
+	} else
+	{
+	strncat(res, s2, n);
+	}
+	
+	return (res);
+}
