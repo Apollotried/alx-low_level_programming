68c59a47e234f30d88aa50da82e7187ff1705673
Apollotried <safeapollogemail@gmail.com>
2023-04-17 12:55:29 -0700

    test

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,25 +12,25 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	} else
 	{
-		printf("Name: %s", d->name);
+		printf("Name: %s\n", d->name);
 	};
 
-	if (d->age == NULL)
+	if (d->age < 0)
 	{
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	} else
 	{
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 	};
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	} else
 	{
-		printf("Owner: %s", d->owner);
+		printf("Owner: %s\n", d->owner);
 	};
 }
