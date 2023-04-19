672b27770a972773fec8970a0130ccec7f1f6c70
Apollotried <safeapollogemail@gmail.com>
2023-04-19 16:38:11 -0700

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,42 @@
-#include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: Always 0 (Success)
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int num1, num2;
+	char *op;
 
-	if (argc != 2)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(98);
 	}
 
-	bytes = atoi(argv[1]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (bytes < 0)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(2);
+		exit(99);
 	}
 
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+		printf("Error\n");
+		exit(100);
 	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
-}
+
