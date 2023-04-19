f98dd68761000c90ba06d2bd298bd818a61c1330
Apollotried <safeapollogemail@gmail.com>
2023-04-19 15:26:08 -0700

    test

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp ==NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 	if (size <= 0)
 		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]))
-			return i;
+			return (i);
 		i++;
 	}
 	return (-1);
