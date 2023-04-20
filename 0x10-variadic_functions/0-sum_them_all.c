cb84c9269c9793e787bb995f57a88cd9da94620f
Apollotried <safeapollogemail@gmail.com>
2023-04-20 13:13:57 -0700

    test

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - the sum of args
+ * @n: the number of args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int total = 0;
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		int arg = va_arg(args, int);
+		tottal += arg;
+	}
+	va_end(args);
+	return (total);
+}
