4c42bd42768bbdf026a852089914a20b52424c82
Apollotried <safeapollogemail@gmail.com>
2023-04-20 13:22:59 -0700

    test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 /**
  * sum_them_all - the sum of args
  * @n: the number of args
+ * Return: int
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -12,13 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0 ; i < n ; i++)
 	{
 		int arg = va_arg(args, int);
+
 		total += arg;
 	}
 	va_end(args);
+
 	return (total);
 }
