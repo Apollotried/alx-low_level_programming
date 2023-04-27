1549632a9f46da219d6d7bf88579ccbb0a057de5
apollotried <safeapollogemail@gmail.com>
2023-04-27 09:55:24 -0700

    test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	{
 		if (temp->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		} else
 		{
 			printf("[%u] %s\n", temp->len, temp->str);
