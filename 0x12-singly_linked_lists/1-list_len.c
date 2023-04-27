c41fc78ab2432b5c0ae35ef3b1f96ab302a323b4
apollotried <safeapollogemail@gmail.com>
2023-04-27 12:44:56 -0700

    second task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len -  returns the number of elements
+ * @h: the header pointer
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h  = h->next
+	}
+
+	return (size);
+}
