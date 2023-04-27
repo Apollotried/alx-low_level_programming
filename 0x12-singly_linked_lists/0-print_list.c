6198805fb2d3a9a657be699e6329ff4c326fc107
apollotried <safeapollogemail@gmail.com>
2023-04-27 09:27:05 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - prints all elements
+ * @h: the head
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *temp;
+	size_t size = 0;
+
+	temp = h;
+
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)");
+		} else
+		{
+			printf("[%d] %s", temp->len, temp->str);
+		};
+		size++;
+		temp = temp->next;
+	}
+	return (size);
+}
