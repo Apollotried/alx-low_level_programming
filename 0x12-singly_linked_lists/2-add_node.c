50d05daf53d82f8a8b778cc74102a015270832e3
apollotried <safeapollogemail@gmail.com>
2023-04-27 13:12:56 -0700

    thied task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - adds to the beginning
+ * @head: the head
+ * @str: the new node's string
+ * Return: list_t*
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t temp;
+
+	temp = malloc(sizeof(list_t));
+
+	if (!temp)
+		return (NULL);
+
+	temp->str = strdup(str);
+	temp->next = *head;
+	temp->len = strlen(str);
+	*head = temp;
+
+	return (*head);
+}
