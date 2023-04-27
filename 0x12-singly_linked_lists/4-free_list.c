258b4bd194dbaffade4216072bee3a163b85bc50
apollotried <safeapollogemail@gmail.com>
2023-04-27 14:01:11 -0700

    last task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - free the list
+ * @head: the head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
