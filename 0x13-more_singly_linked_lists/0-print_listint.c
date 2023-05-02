d3b1b85975ea3c8b6b5a1fec568a95870d7c5c1d
apollotried <safeapollogemail@gmail.com>
2023-05-02 10:23:40 -0700

    first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements
+ * @h: the head
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t size;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
