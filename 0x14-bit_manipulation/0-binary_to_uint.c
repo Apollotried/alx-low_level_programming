4e7e5b7b6664c224ba995202a6bb54ab513214e0
apollotried <safeapollogemail@gmail.com>
2023-05-04 12:05:59 -0700

    first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int size = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
 
-	if (b[size] == '\0')
-		return (0);
-
-	while ((b[size] == '0') || (b[size] == '1'))
+	for (i = 0 ; b[i] ; i++)
 	{
-		result <<= 1;
-		result += b[size] - '0';
-		size++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
