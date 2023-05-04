c9d120d4d79e10323cd18ebe099706e8767ee668
apollotried <safeapollogemail@gmail.com>
2023-05-04 11:42:29 -0700

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts to an unsigned int
+ * @b: the string to manipulate
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int size = 0;
+
+	if (b[size] == '\0')
+		return (0);
+
+	while ((b[size] == '0') || (b[size] == '1'))
+	{
+		result <<= 1;
+		result += b[size] - '0';
+		size++;
+	}
+
+	return (result);
+}
