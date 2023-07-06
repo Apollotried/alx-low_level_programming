6c9f72a6e72dee3d045786ec18dbccc6724046a8
Apollotried <safeapollogemail@gmail.com>
2023-07-06 15:34:06 -0700

    second task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary
+ * @n: number
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar('0' + (1 & n));
+	}
+	else
+	{
+		_putchar('0');
+	}
+
