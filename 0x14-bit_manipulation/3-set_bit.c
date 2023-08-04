0a5728c2acc81c32b5bdfade54ee25aa3aadfc0e
Apollotried <safeapollogemail@gmail.com>
2023-08-04 08:37:10 -0700

    update

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * set_bit - sets value to 1
+ * @n: pointer to the number
+ * @index: index
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1)
+	}
+	*n = (*n | (1UL << index));
+
+	return (1);
+	
+}
