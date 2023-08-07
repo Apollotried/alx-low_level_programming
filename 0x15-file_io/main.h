fa5589bfa0ee1f65e49b4691f6ea00235d1b693c
Apollotried <safeapollogemail@gmail.com>
2023-08-07 08:01:13 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <elf.h>
+#include <string.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
