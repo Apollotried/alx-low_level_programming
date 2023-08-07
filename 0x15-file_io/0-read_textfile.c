5685e2de22069df135397313b5b77884718f3a3b
Apollotried <safeapollogemail@gmail.com>
2023-08-07 07:58:45 -0700

    first

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile - read and print
+ * @filename: name of file
+ * @letters: nb of letters
+ * Return: ssize_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, b, n;
+	char *buf;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	b = read(fd, buf, letters);
+	a = write(STDOUT_FILENO, buf, b);
+
+	free(buffer);
+	close(fd);
+
+	return (a);
+}
