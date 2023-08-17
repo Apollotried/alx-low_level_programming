bf847f6a0890adfd8dc455d1528b12314fc03961
Apollotried <safeapollogemail@gmail.com>
2023-08-17 16:11:34 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,9 @@ size_t print_dlistint(const dlistint_t *h)
 	int count;
 
 	count = 0;
+	if (h == NULL)
+		return (count);
+
 	while (h != NULL)
 	{
 		printf("%d", h->n);
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t print_dlistint(const dlistint_t *h);
 
