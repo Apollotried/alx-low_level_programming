e44ae038d7b146c182bb01f549b2a17d9c43b07a
Apollotried <safeapollogemail@gmail.com>
2023-08-17 16:07:12 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_dlistint - prints elements
  * @h: head of the list
@@ -7,7 +8,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
-	
+
 	count = 0;
 	while (h != NULL)
 	{
