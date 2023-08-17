c40ddc1981915e4dd02a70ff7f0a5d5540b1255a
Apollotried <safeapollogemail@gmail.com>
2023-08-17 15:39:31 -0700

    update

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints elements
+ * @h: head of the list
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+	
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
