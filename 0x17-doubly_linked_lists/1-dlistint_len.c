a7b63076b540d6db0db7a718090a1a868703e13c
Apollotried <safeapollogemail@gmail.com>
2023-08-18 16:12:58 -0700

    update

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - nbr of elements
+ * @h: pointer to the header node
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+
+	if (!h)
+		return (count);
+
+	while (h)
+	{
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
