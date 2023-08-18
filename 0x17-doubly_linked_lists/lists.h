d13dbd57ff485595e7987785df1ff7e39cc3ed5d
Apollotried <safeapollogemail@gmail.com>
2023-08-18 15:53:54 -0700

    update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: int
+ * @prev: pointer to prev node
+ * @next: pointer to next node
+ *
+ * Description: dll structure
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 
 
